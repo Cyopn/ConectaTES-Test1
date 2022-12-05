30f7d0c9a8462894865a4f0f2ce065bff8d45bfb
Cyopn <frijolesconcatsu@gmail.com>
2022-12-04 21:11:14 -0600

    Script
    Se agrega los eventos para el popup

--- js/script.js
+++ js/script.js
@@ -1,26 +1,6 @@
-const pswrdField = document.querySelector(".form input[type='password']"),
-    toggleIcon = document.querySelector(".form .field i");
-
-toggleIcon.onclick = () => {
-    if (pswrdField.type === "password") {
-        pswrdField.type = "text";
-        toggleIcon.classList.add("active");
-    } else {
-        pswrdField.type = "password";
-        toggleIcon.classList.remove("active");
-    }
-}
-
-
-var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-    overlay = document.getElementById('overlay'),
+var overlay = document.getElementById('overlay'),
     popup = document.getElementById('popup'),
     btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-
-btnAbrirPopup.addEventListener('click', function () {
-    overlay.classList.add('active');
-    popup.classList.add('active');
-});
 
 btnCerrarPopup.addEventListener('click', function (e) {
     e.preventDefault();
@@ -28,9 +8,11 @@ btnCerrarPopup.addEventListener('click', function (e) {
     popup.classList.remove('active');
 });
 
-
-
-
 function cn() {
     window.open("./dashboard.html", "_self")
 } 
+
+function pop(){
+    overlay.classList.add('active');
+    popup.classList.add('active');
+}
